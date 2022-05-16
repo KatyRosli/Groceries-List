e0de2fd343a21128294dd75d4a55321d87fc99ac
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2022-05-16 18:43:53 +0200

    added useState & ChangeEvent

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import React, {FC} from 'react';
+import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
 
 const App : FC = () => {
+
+  const [list, setList] = useState<string>("");
+  const [quantity, setQuantity] = useState<number>(0);
+  const [groceries, setGroceriesList] = useState([]);
+
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement> ) => {
+  if (event.target.name === "list") {
+    setList(event.target.value)
+  } else {
+    setQuantity(Number(event.target.value));
+  }
+    
+  };
   return ( 
   <div className="App">
     <div className="header">
       <div className="inputContainer">
-      <input type="text" placeholder="List..."/>
-      <input type="text" placeholder="Quantity..."/>
+      <input type="text" placeholder="List..." name="list" onChange={handleChange}/>
+      <input type="text" placeholder="Quantity..." name="deadline" onChange={handleChange}/>
       <button>Add to list</button>
     </div>
     <div className="Grogeries List"></div>
