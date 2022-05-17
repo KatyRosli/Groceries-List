deac4539c18629636b67dd16ba92a673d81e22f6
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2022-05-17 17:05:30 +0200

    created interfaces.ts file

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,34 @@
 import React, {FC, ChangeEvent, useState} from 'react';
 import './App.css';
+import {IList} from './Interfaces'
 
 const App : FC = () => {
 
   const [list, setList] = useState<string>("");
   const [quantity, setQuantity] = useState<number>(0);
-  const [groceries, setGroceriesList] = useState([]);
+  const [groceriesList, setGroceriesList] = useState<IList[]>([]);
 
 
-  const handleChange = (event: ChangeEvent<HTMLInputElement> ) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
   if (event.target.name === "list") {
     setList(event.target.value)
   } else {
     setQuantity(Number(event.target.value));
   }
-    
   };
+
+  const addList = (): void => {
+    const newList = {listName: list, quantity: quantity };
+    setGroceriesList([...groceriesList, newList]);
+  };
+  
   return ( 
   <div className="App">
     <div className="header">
       <div className="inputContainer">
       <input type="text" placeholder="List..." name="list" onChange={handleChange}/>
-      <input type="text" placeholder="Quantity..." name="deadline" onChange={handleChange}/>
-      <button>Add to list</button>
+      <input type="text" placeholder="Quantity..." name="quantity" onChange={handleChange}/>
+      <button onClick={addList}>Add to list</button>
     </div>
     <div className="Grogeries List"></div>
     </div>
