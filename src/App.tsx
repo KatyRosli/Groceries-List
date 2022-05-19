7f231651b57ce1a8852126b572b0c467cbb0b528
Katy Rosli <96566968+KatyRosli@users.noreply.github.com>
2022-05-19 10:34:11 +0200

    added footer on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -60,4 +60,25 @@ const App : FC = () => {
   );
 }
 
+<footer>
+<a
+  href="https://github.com/KatyRosli/Groceries-List"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="github"
+>
+  Open sourced on Github{" "}
+</a>
+by
+<a
+  href="https://www.katyrosli.com"
+  target="_blank"
+  rel="nopener noreferrer"
+  className="name"
+>
+  {" "}
+  Katy Rosli
+</a>
+</footer>
+
 export default App;
